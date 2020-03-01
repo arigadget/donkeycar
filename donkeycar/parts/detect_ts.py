830f550c413b803f1b029e8c1dfe44cd799443de
arigadget <t.ari3776@nifty.com>
2020-03-01 18:38:12 +0900

    modify detect_ts

--- donkeycar/parts/detect_ts.py
+++ donkeycar/parts/detect_ts.py
@@ -16,7 +16,7 @@ class DetectTS():
     def __init__(self, model_path=None, label_path=None):
         from edgetpu.detection.engine import DetectionEngine
         # Initialize engine.
-        print('load traffic sign model')
+        #print('load traffic sign model')
         self.engine = DetectionEngine(model_path)
         self.labels = self.ReadLabelFile(label_path)
         
@@ -24,26 +24,35 @@ class DetectTS():
         self.traffic_sign = None
 
     def inference_traffic_sign(self, image, angle, throttle):
-        # Run inference.
         import time
+        import numpy
+        from PIL import Image
+
+        self.new_angle = angle
+        self.new_throttle = throttle
+
+        # Run inference
+        pilImg = Image.fromarray(numpy.uint8(image))
         start_time = time.perf_counter()
-        ans = self.engine.DetectWithImage(image, threshold=0.05, keep_aspect_ratio=True,
-                                          relative_coord=False, top_k=3)
+        ans = self.engine.detect_with_image(pilImg, threshold=0.7, keep_aspect_ratio=True,
+                                          relative_coord=False, top_k=1)
         end_time =  time.perf_counter()
-        print('Inference time:{:.7}'.format(end_time - start_time))
+        #print('Inference time:{:.7}'.format(end_time - start_time))
 
         # Display result.
         if ans:
             for obj in ans:
-                print ('-----------------------------------------')
+                #print ('-----------------------------------------')
                 if self.labels:
-                    print(self.labels[obj.label_id])
-                print ('score = ', obj.score)
-                #box = obj.bounding_box.flatten().tolist()
-                #print ('box = ', box)
-
-        self.new_angle = angle
-        self.new_throttle = throttle
+                    #print(self.labels[obj.label_id],'   ',obj.score)
+                    traffic_sign = self.labels[obj.label_id]
+                    if traffic_sign == "stop" :
+                        print("stop")
+                        self.new_throttle = 0.0
+                    elif traffic_sign == "pause":
+                        self.new_angle = 1.0
+                    else:
+                        self.new_throttle = throttle
 
     def update(self):
         while self.on:
@@ -53,9 +62,9 @@ class DetectTS():
         self.run_threaded(image, angle, throttle)
 
     def run_threaded(self, image, angle, throttle):
-        self.angle = angle
-        self.throttle = throttle
-        if image != None
+        self.new_angle = angle
+        self.new_throttle = throttle
+        if image is not None:
             self.inference_traffic_sign(image, angle, throttle)
         return self.new_angle, self.new_throttle
 
